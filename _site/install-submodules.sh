e3f1828ab469e102dcd74e928206e67ed86afe03
Matthew Boles <mboles@brightcove.com>
2022-05-18 14:52:34 -0400

    test

---
+++ _site/install-submodules.sh
@@ -0,0 +1,5 @@
+git submodule add https://github.com/BrightcoveLearning/s-scripts
+cd _includes
+git submodule add https://github.com/BrightcoveLearning/s-jekyll-page-includes
+cd ..
+git submodule update --init --recursive
