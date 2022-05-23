92dc9d8c67c229c248173a4d3156dba056b40ab8
Robert Crooks <rcrooks@brightcove.com>
2022-05-23 16:19:45 -0400

    adding support submodule

--- install-submodules.sh
+++ install-submodules.sh
@@ -1,6 +1,8 @@
 git submodule add https://github.com/BrightcoveLearning/s-scripts
+git submodule add https://github.com/BrightcoveLearning/s-support
 cd _includes
 git submodule add https://github.com/BrightcoveLearning/s-jekyll-page-includes
 git submodule add https://github.com/BrightcoveLearning/s-feather-img
+git submodule add https://github.com/BrightcoveLearning/s-support-img
 cd ..
 git submodule update --init --recursive
