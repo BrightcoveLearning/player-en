d19e970cd7f23e4f6ebc4d0cee5894abf1581b91
Matthew Boles <mboles@brightcove.com>
2022-05-25 13:22:47 -0400

    rearrange submodules

--- install-submodules.sh
+++ install-submodules.sh
@@ -1,8 +1,10 @@
 git submodule add https://github.com/BrightcoveLearning/s-scripts
-git submodule add https://github.com/BrightcoveLearning/s-support
 cd _includes
+git submodule add https://github.com/BrightcoveLearning/s-support
 git submodule add https://github.com/BrightcoveLearning/s-jekyll-page-includes
-git submodule add https://github.com/BrightcoveLearning/s-feather-img
+cd ..
+cd assets/images
 git submodule add https://github.com/BrightcoveLearning/s-support-img
+git submodule add https://github.com/BrightcoveLearning/s-feather-img
 cd ..
 git submodule update --init --recursive
